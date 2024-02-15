4f8768a6ff430cdc6a44e4c25f44bc9a3cf85da8
Sospeter <sosmogendi@gmail.com>
2024-02-15 19:51:44 +0000

    Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,49 +1,52 @@
 #!/usr/bin/env python3
-""" Module for API Authentication
-"""
+""" Module for Authentication Handling """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
+
 
 class Auth:
-    """ Class to Handle API Authentication """
+    """ Class for managing API authentication """
 
-    def require_auth(self, endpoint_path: str, excluded_paths: List[str]) -> bool:
-        """ Validate if authentication is required for the given endpoint path """
-        if endpoint_path is None or excluded_paths is None or excluded_paths == []:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Checks if authentication is required for a given path """
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
 
-        path_length = len(endpoint_path)
-        if path_length == 0:
-            return True
+        is_slash_path = path[-1] == '/'  # Check if the path ends with a slash
 
-        ends_with_slash = True if endpoint_path[path_length - 1] == '/' else False
-
-        temp_path = endpoint_path
-        if not ends_with_slash:
-            temp_path += '/'
-
-        for excluded_path in excluded_paths:
-            excluded_length = len(excluded_path)
-            if excluded_length == 0:
-                continue
-
-            if excluded_path[excluded_length - 1] != '*':
-                if temp_path == excluded_path:
+        tmp_path = path if is_slash_path else path + '/'
+        
+        for exc in excluded_paths:
+            if exc.endswith('*'):
+                if exc[:-1] == path[:len(exc) - 1]:
                     return False
-            else:
-                if excluded_path[:-1] == endpoint_path[:excluded_length - 1]:
-                    return False
+            elif tmp_path == exc:
+                return False
 
         return True
 
-    def authorization_header(self, request_obj=None) -> str:
-        """ Retrieve the Authorization header from the Flask request object """
-        if request_obj is None:
+    def authorization_header(self, request=None) -> str:
+        """ Retrieves the Authorization header from the request """
+        if request is None:
             return None
 
-        return request_obj.headers.get("Authorization", None)
+        return request.headers.get("Authorization", None)
 
-    def current_user(self, request_obj=None) -> TypeVar('User'):
-        """ Validate the current user """
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Validates the current user """
         return None
 
+    def session_cookie(self, request=None):
+        """ Returns the value of the session cookie from the request """
+        if request is None:
+            return None
+
+        session_name = getenv("SESSION_NAME")
+
+        if session_name is None:
+            return None
+
+        session_id = request.cookies.get(session_name)
+
+        return session_id
