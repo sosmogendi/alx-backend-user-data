99a6b2802eb8bb7fa587dd1f848215c0205f382b
Sospeter <sosmogendi@gmail.com>
2024-02-15 20:13:24 +0000

    Sessions in database

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,70 @@
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from datetime import datetime, timedelta
+from models.user_session import UserSession
+
+class SessionDBAuth(SessionExpAuth):
+    """Class for Session Authentication with Database Storage"""
+
+    def create_session(self, user_id=None):
+        """Create session and store in the database"""
+        session_id = super().create_session(user_id)
+
+        if session_id is None:
+            return None
+
+        kwargs = {'user_id': user_id, 'session_id': session_id}
+        user_session = UserSession(**kwargs)
+        user_session.save()
+        UserSession.save_to_file()
+
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """Retrieve User ID associated with a Session ID from the Database"""
+        if session_id is None:
+            return None
+
+        UserSession.load_from_file()
+        user_sessions = UserSession.search({'session_id': session_id})
+
+        if not user_sessions:
+            return None
+
+        user_session = user_sessions[0]
+
+        expired_time = user_session.created_at + \
+            timedelta(seconds=self.session_duration)
+
+        if expired_time < datetime.utcnow():
+            return None
+
+        return user_session.user_id
+
+    def destroy_session(self, request=None):
+        """Remove Session from the Database"""
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        user_sessions = UserSession.search({'session_id': session_id})
+
+        if not user_sessions:
+            return False
+
+        user_session = user_sessions[0]
+
+        try:
+            user_session.remove()
+            UserSession.save_to_file()
+        except Exception:
+            return False
+
+        return True
