85616fd485898c936e16a57e3f71be6aff7f83a8
Sospeter <sosmogendi@gmail.com>
2024-02-15 19:54:47 +0000

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,10 +42,14 @@ def validate_request() -> str:
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']  # Add the new path here
 
     if not authentication.require_auth(request.path, excluded_paths):
         return
+
+    if authentication.authorization_header(request) is None and authentication.session_cookie(request) is None:
+        abort(401)
 
     if authentication.authorization_header(request) is None:
         abort(401)
